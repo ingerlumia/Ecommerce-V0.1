43f85273583d893bad3213e3126d11c0e98988ef
ingerlumia <125810742+ingerlumia@users.noreply.github.com>
2026-01-27 23:33:45 +0530

    Refactor ProductedRoute for loading state handling

--- frontend/src/components/Route/ProductedRoute.js
+++ frontend/src/components/Route/ProductedRoute.js
@@ -4,27 +4,27 @@ import Loader from "../layouts/Loader";
 
 export default function ProductedRoute({children, isAdmin, isSeller,isManager}){
     const { isAuthenticated, loading, user }= useSelector(state => state.authState);
-
-    if(!isAuthenticated && !loading){
+    if(loading){
+        return <Loader/>
+    }
+    if(!isAuthenticated ){
        return <Navigate to={"/Login"}/>
     }
 
     if(isAuthenticated){
-        if(isAdmin == true && user.role !== 'admin'){
+        if(isAdmin == true && user?.role !== 'admin'){
             return <Navigate to={'/'} />
         }
-        if(isSeller == true && user.role !== 'seller'){
+        if(isSeller == true && user?.role !== 'seller'){
             return <Navigate to={'/'} />
         }
-        if(isManager == true && user.role !== 'manager'){
+        if(isManager == true && user?.role !== 'manager'){
             return <Navigate to={'/'} />
         }
         return children;
     }
 
-    if(loading){
-        return <Loader/>
-    }
 
-    return children;
+
+    return <Loader/>;
 }
