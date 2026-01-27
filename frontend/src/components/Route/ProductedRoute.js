c7e4794e4bb340de6b462e6bc176a57c9edf4844
ingerlumia <125810742+ingerlumia@users.noreply.github.com>
2026-01-27 23:56:28 +0530

    Update ProductedRoute.js

--- frontend/src/components/Route/ProductedRoute.js
+++ frontend/src/components/Route/ProductedRoute.js
@@ -4,28 +4,29 @@ import Loader from "../layouts/Loader";
 
 export default function ProductedRoute({children, isAdmin, isSeller,isManager}){
     const { isAuthenticated, loading, user }= useSelector(state => state.authState);
-    if(loading){
-        return <Loader/>
-    }
-    if(!isAuthenticated ){
+
+    if(!isAuthenticated && !loading){
        return <Navigate to={"/Login"}/>
     }
 
     if(isAuthenticated){
-        if(isAdmin == true && user?.role !== 'admin'){
+        if(isAdmin == true && user.role !== 'admin'){
             return <Navigate to={'/'} />
         }
-        if(isSeller == true && user?.role !== 'seller'){
+        if(isSeller == true && user.role !== 'seller'){
             return <Navigate to={'/'} />
         }
-        if(isManager == true && user?.role !== 'manager'){
+        if(isManager == true && user.role !== 'manager'){
             return <Navigate to={'/'} />
         }
         return children;
     }
 
-
+    if(loading){
+        return <Loader/>
+    }
 
     return children;
 }
+
 
