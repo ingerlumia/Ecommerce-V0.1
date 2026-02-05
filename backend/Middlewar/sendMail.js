4e00db920aa1eddfca3899b1fa4a773592aa49e7
ingerlumia <125810742+ingerlumia@users.noreply.github.com>
2026-02-05 23:27:52 +0530

    Update sendMail.js

--- backend/Middlewar/sendMail.js
+++ backend/Middlewar/sendMail.js
@@ -13,7 +13,7 @@ const sendMail = async (email, subject, text) => {
     greetingTimeout: 5000,
     });
     await transport.sendMail({
-      from: process.env.GUSER,
+      from: 'parwinparwin2500@gmail.com',
       to: email,
       subject,
       text,
@@ -24,6 +24,7 @@ const sendMail = async (email, subject, text) => {
 };
 
 export default sendMail;
+
 
 
 
